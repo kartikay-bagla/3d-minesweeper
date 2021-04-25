4ff8773b41d08233978edcf8418a30400512d1b1
gunagya <gunagya@github.com>
2021-04-25 20:48:51 +0530

    Created scene for map testing

---
+++ Assets/Scripts/GameControllerMaze.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerMaze : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject mazePrefab;
+	private GameObject mazeInstance;
+
+    void Start()
+    {
+        BeginGame();
+    }
+
+    private void BeginGame() { 
+        mazeInstance = Instantiate(mazePrefab) as GameObject;
+        mazeInstance.GetComponent<MazeGenerator>().Generate();
+        Instantiate(player, new Vector3(0, 1, 0), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    public GameObject roomPrefab;
+    public int sizeX, sizeZ;
+    public int increment = 2;
+    private GameObject[,] rooms;
+    void Start()
+    {
+        
+    }
+
+    public void Generate () {
+		rooms = new GameObject[sizeX, sizeZ];
+		for (int x = 0; x < sizeX; x++) {
+			for (int z = 0; z < sizeZ; z++) {
+				CreateRoom(x, z);
+			}
+		}
+	}
+
+	private void CreateRoom (int x, int z) {
+		GameObject newRoom = Instantiate(roomPrefab, new Vector3(x*increment, 0, z*increment), Quaternion.identity) as GameObject;
+		rooms[x, z] = newRoom;
+		newRoom.name = "Room " + x + ", " + z;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
